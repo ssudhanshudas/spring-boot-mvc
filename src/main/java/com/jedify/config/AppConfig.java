abf45d3f6e8dc9dd37cab15b2e22ca2ef73d4d7e
j1013575 <sudhanshu.das@jda.com>
2015-10-31 23:46:12 +0530

    Initial infrastructure codebase

---
+++ src/main/java/com/jedify/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.jedify.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by j1013575 on 10/31/2015.
+ */
+@Configuration
+public class AppConfig extends WebMvcConfigurerAdapter{
+    @Bean
+    public ViewResolver getViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+}
