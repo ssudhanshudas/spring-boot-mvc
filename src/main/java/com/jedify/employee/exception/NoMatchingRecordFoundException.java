6c9c9132631ec871088a02b74a932ff5d04ba023
j1013575 <sudhanshu.das@jda.com>
2015-10-31 23:46:47 +0530

    Initial infrastructure codebase

---
+++ src/main/java/com/jedify/employee/exception/NoMatchingRecordFoundException.java
@@ -0,0 +1,10 @@
+package com.jedify.employee.exception;
+
+/**
+ * Created by j1013575 on 10/31/2015.
+ */
+public class NoMatchingRecordFoundException extends Throwable {
+    public NoMatchingRecordFoundException(String s) {
+        super(s);
+    }
+}
